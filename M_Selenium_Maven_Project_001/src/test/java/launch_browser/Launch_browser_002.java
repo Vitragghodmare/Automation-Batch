3988216406ce75d7edfe7e758b2fdb20e7bd7476
Vitragghodmare <155629656+Vitragghodmare@users.noreply.github.com>
2024-01-08 15:10:02 +0530

    update maven project

--- M_Selenium_Maven_Project_001/src/test/java/launch_browser/Launch_browser_002.java
+++ M_Selenium_Maven_Project_001/src/test/java/launch_browser/Launch_browser_002.java
@@ -1,5 +1,7 @@
 package launch_browser;
 
+import org.openqa.selenium.By;
+
 import base_class.Base_Class;
 
 public class Launch_browser_002 extends Base_Class{
@@ -8,10 +10,10 @@ public class Launch_browser_002 extends Base_Class{
 		
 		launch_browser("chrome");
 		navigate("https://www.amazon.in/");
-		
-		
-		
-		
+		driver.findElement(By.xpath("//input[@id=\"twotabsearchtextbox\"]")).sendKeys("bags for men");
+		driver.findElement(By.xpath("//input[@id=\"nav-search-submit-button\"]")).click();
+		driver.findElement(By.xpath("//img[@class=\"s-image\"]")).click();
+		driver.findElement(By.xpath("//input[@id=\"add-to-cart-button\"]")).click();
 	}
 	
 }
